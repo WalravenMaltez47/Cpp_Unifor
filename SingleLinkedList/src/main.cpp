67129eb9dc49a600e88331410e22ac2be72f62da
WalravenMaltez47 <pamaltez@hotmail.com>
2025-03-05 00:59:33 -0300

    hh

--- SingleLinkedList/src/main.cpp
+++ SingleLinkedList/src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-class Node {
-    public:
+struct Node { 
     int data;
     Node *next;
 
     Node(int data) : data(data), next(nullptr) {}
+    
 };
+ 
 
 class List {
     public:
