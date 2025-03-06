18a89e7d525a12e89216b97074886798d64efc44
WalravenMaltez47 <pamaltez@hotmail.com>
2025-03-05 22:31:05 -0300

    show

--- SingleLinkedList/src/main.cpp
+++ SingleLinkedList/src/main.cpp
@@ -1,78 +1,143 @@
 #include <iostream>
 
-struct Node { 
-    int data;
+template <typename T>
+struct Node
+{
+    T data;
     Node *next;
 
-    Node(int data) : data(data), next(nullptr) {}
-    
+    Node(T data) : data(data), next(nullptr) {}
 };
- 
-class List {
-    public:
-    Node *head;
-    Node *tail;
+
+template <typename T>
+class LinkedList
+{
+private:
+    Node<T> *head;
+    Node<T> *tail;
     int size;
 
-    List() : head(nullptr), tail(nullptr), size(0) {}
+public:
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
 
-    ~List() {
-        Node *current = head;
-        while (current != nullptr) {
-            Node *nextNode = current->next;
+    ~LinkedList()
+    {
+        Node<T> *current = head;
+        while (current != nullptr)
+        {
+            Node<T> *nextNode = current->next;
             delete current;
             current = nextNode;
         }
     }
 
-    int insertAtBeginning(int data) {
-        Node *newNode = new Node(data);
+    T insert_At_Beginning(T data)
+    {
+        Node<T> *newNode = new Node<T>(data);
 
-        if (head == nullptr) {
+        if (head == nullptr)
+        {
             head = newNode;
             tail = newNode;
-        } else {
+        }
+
+        else
+        {
             newNode->next = head;
             head = newNode;
         }
 
         size++;
-        return 0;
+        return head->data;
     }
 
-    int insertAtEnd(int data) {
-        Node *newNode = new Node(data);
+    T insert_At_End(T data)
+    {
+        if (head == nullptr)
+        {
+            return insert_At_Beginning(data);
+        }
 
-        if (head == nullptr) {
-            head = newNode;
-            tail = newNode;
-        } else {
+        else
+        {
+            Node<T> *newNode = new Node<T>(data);
+
             tail->next = newNode;
             tail = newNode;
         }
 
         size++;
-        return 0;
+        return tail->data;
+    }
+
+    void insert_At_Position(T data, int position)
+    {
+        if (position < 1 || position > size + 1)
+        {
+            std::cerr << "Invalid position!" << std::endl;
+            return;
+        }
+
+        Node<T> *newNode = new Node<T>(data);
+
+        if (position == 1)
+        {
+            newNode->next = head;
+            head = newNode;
+            if (tail == nullptr)
+            {
+                tail = newNode;
+            }
+        }
+
+        else if (position == size + 1)
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            Node<T> *current = head;
+            for (int i = 1; i < position - 1; ++i)
+            {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+
+        size++;
+    }
+
+    int getSize() const
+    {
+        return size;
+    }
+
+    void print()
+    {
+        Node<T> *current = head;
+
+        while (current != nullptr)
+        {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
     }
 };
+    int main()
+    {
+        LinkedList<double> list;
+        list.insert_At_Beginning(10);
+        list.insert_At_Beginning(20);
+        list.insert_At_End(30);
+        list.insert_At_End(40);
+        list.insert_At_Position(25, 3);
 
-void printList(List *list) {
-    Node *current = list->head;
-    while (current != nullptr) {
-        std::cout << current->data << " ";
-        current = current->next;
+        std::cout << list.getSize() << std::endl;
+
+        list.print();
+        
+        return 0;
     }
-    std::cout << std::endl;
-}
-
-int main() {
-    List *list = new List();
-    list->insertAtBeginning(1);
-    list->insertAtBeginning(2);
-    list->insertAtEnd(3);
-
-    printList(list);
-
-    delete list; 
-    return 0;
-}
